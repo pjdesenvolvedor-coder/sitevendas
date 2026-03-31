ecfdb7835755e7f598e90c7feda64e48b0be0a97
Pj Contas <pjassinaturas@gmail.com>
2026-03-31 18:02:46 +0000

    Certo Fale comigo em portugues! QUero que mude a fonte do site todo apar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['PT Sans', 'sans-serif'],
-        headline: ['Playfair Display', 'serif'],
+        body: ['Inter', 'sans-serif'],
+        headline: ['Bebas Neue', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
